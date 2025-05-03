77dadff1b7afeb9d2cec8576031864fad4e522d3
Devesh <devesh.bulani@gmail.com>
2025-05-03 16:18:25 -0500

    Initial Commit

---
+++ login/loginApp.swift
@@ -0,0 +1,17 @@
+//
+//  loginApp.swift
+//  login
+//
+//  Created by Devesh Bulani on 5/3/25.
+//
+
+import SwiftUI
+
+@main
+struct loginApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
